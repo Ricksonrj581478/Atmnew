a878360c183b94c95c14d087fa2743af5bc6d01c
Ricksonrj98514 <Rraju5814@conestogac.on.ca>
2024-11-20 13:42:25 -0800

    Add files via upload

---
+++ TransactionHistoryTransaction.cs
@@ -0,0 +1,13 @@
+﻿using System.Transactions;
+
+public class TransactionHistoryTransaction : Transaction
+{
+    protected override void PerformTransaction(BankAccount account)
+    {
+        Console.WriteLine("\nTransaction History:");
+        foreach (var transaction in account.TransactionHistory)
+        {
+            Console.WriteLine(transaction);
+        }
+    }
+}
---
+++ WithdrawTransaction.cs
@@ -0,0 +1,32 @@
+﻿using System.Transactions;
+
+public class WithdrawTransaction : Transaction
+{
+    private double amount;
+
+    public WithdrawTransaction(double amount)
+    {
+        this.amount = amount;
+    }
+
+    // Constructor with no amount specified, it will ask for the amount at runtime
+    public WithdrawTransaction() { }
+
+    protected override void PerformTransaction(BankAccount account)
+    {
+        if (amount == 0)
+        {
+            if (!account.Withdraw()) // Ask the user for the withdrawal amount
+            {
+                Console.WriteLine("Insufficient funds.");
+            }
+        }
+        else
+        {
+            if (!account.Withdraw(amount)) // Use the specified amount
+            {
+                Console.WriteLine("Insufficient funds.");
+            }
+        }
+    }
+}
